477769513d958700434a3098bf8609f55cd212a0
Joakim Runeberg <joakim.runeberg@gmail.com>
2015-05-21 16:36:54 +0300

    worker updates

--- lib/capistrano/tasks/workers.rb
+++ lib/capistrano/tasks/workers.rb
@@ -1,34 +1,30 @@
-namespace :puma do
-  namespace :workers do
-    desc 'Add a worker'
-    task :count do
-      on roles (fetch(:puma_role)) do
+Capistrano::Configuration.instance.load do
+  namespace :puma do
+    namespace :workers do
+      desc 'Add a worker'
+      task :count, roles: fetch(:puma_role) do
         #TODO
         # cleanup
         # add host name/ip
-        workers_count = capture("ps ax | grep -c 'puma: cluster worker: `cat  #{fetch(:puma_pid)}`'").to_i - 1
-        log  "Workers count : #{workers_count}"
+        workers_count = capture("ps ax | grep -c 'puma: cluster worker [0-9]: `cat  #{fetch(:puma_pid)}`'").to_i
+        logger.info  "Workers count : #{workers_count}"
       end
-    end
 
-    # TODO
-    # Add/remove workers to specific host/s
-    # Define  # of workers to add/remove
-    # Refactor
-    desc 'Worker++'
-    task :more do
-      on roles (fetch(:puma_role)) do
-        execute("kill -TTIN `cat  #{fetch(:puma_pid)}`")
+      # TODO
+      # Add/remove workers to specific host/s
+      # Define  # of workers to add/remove
+      # Refactor
+      desc 'Worker++'
+      task :more, roles: fetch(:puma_role) do
+          run("kill -TTIN `cat  #{fetch(:puma_pid)}`")
       end
-    end
 
-    desc 'Worker--'
-    task :less do
-      on roles (fetch(:puma_role)) do
-        execute("kill -TTOU `cat  #{fetch(:puma_pid)}`")
+      desc 'Worker--'
+      task :less, roles: fetch(:puma_role) do
+        run("kill -TTOU `cat  #{fetch(:puma_pid)}`")
       end
+
+
     end
-
-
   end
 end
